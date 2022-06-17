2ede80fbe5ed5cf4e09ea36fbf4a49ac8d77c19b
YamasouA <akiakiskyhand@gmail.com>
2022-06-17 16:31:16 +0900

    can work redirect and body tag with class

--- socket_utils.py
+++ socket_utils.py
@@ -50,7 +50,10 @@ def show(body):
         elif c == ">":
             in_angle = False
             text = text[:-1]
-            if text in body_tag:
+            tag = text.split(' ')[0]
+            #print("=== tag ===")
+            #print(tag)
+            if tag in body_tag:
                 in_body = not in_body
             text = ""
             continue
@@ -114,96 +117,113 @@ def chunked_text(body):
     #print(text)
 
 def request(url):
-    is_view_source = False
-    scheme, url, host, path = urlparse(url)
-    if scheme == "view-source":
-        is_view_source = True
+    # リダイレクトの制限
+    for i in range(10):
+        is_view_source = False
         scheme, url, host, path = urlparse(url)
-    s = socket.socket(
-        family=socket.AF_INET,
-        type=socket.SOCK_STREAM,
-        proto=socket.IPPROTO_TCP,
-    )
-    # HTTPSは443ポートを使う
-    if scheme == "http":
-        port = 80
-    elif scheme == "https":
-        port = 443
-    elif scheme == "file":
-        port = 8000
-    if scheme == "https":
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
+        if scheme == "view-source":
+            is_view_source = True
+            scheme, url, host, path = urlparse(url)
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        # HTTPSは443ポートを使う
+        if scheme == "http":
+            port = 80
+        elif scheme == "https":
+            port = 443
+        elif scheme == "file":
+            port = 8000
+        if scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=host)
 
-    # カスタムポート
-    if ":" in host:
-        host, port = host.split(":", 1)
-        port = int(port)
-    #host = "localhost"
-    s.connect((host, port))
-    #print(s)
-    # 改行には\r\nを用いる
-    # リクエストの最後に空行を入れる
-    # データ送信時はバイトとして送るからエンコードが必要
+        # カスタムポート
+        if ":" in host:
+            host, port = host.split(":", 1)
+            port = int(port)
+        #host = "localhost"
+        s.connect((host, port))
+        #print(s)
+        # 改行には\r\nを用いる
+        # リクエストの最後に空行を入れる
+        # データ送信時はバイトとして送るからエンコードが必要
 
-    # リクエストヘッダーを作成
-    method = "GET"
-    http_ver = "HTTP/1.1"
-    request_headers = {}
-    request_headers["Host"] = host
-    request_headers["Connection"] = "close"
-    request_headers["User-Agent"] = "picobr"
-    request_headers["Accept-Encoding"] = "gzip"
-    request_header = "{} {} {}\r\n".format(method, path, http_ver)
-    for key, value in request_headers.items():
-        tmp_str = "{}: {}\r\n".format(key, value)
-        request_header += tmp_str
-    request_header = (request_header + "\r\n").encode("utf-8")
-    #print(request_header)
-    #print(type(request_header))
-    
-    s.send(request_header)
-    
-    #response = s.makefile("r", encoding="utf8", newline="\r\n")
-    response = s.makefile("rb", newline="\r\n")
-    print("response type: ", type(response))
+        # リクエストヘッダーを作成
+        method = "GET"
+        http_ver = "HTTP/1.1"
+        request_headers = {}
+        request_headers["Host"] = host
+        #request_headers["Connection"] = "keep-alive"
+        request_headers["Connection"] = "close"
+        request_headers["User-Agent"] = "picobr"
+        request_headers["Accept-Encoding"] = "gzip"
+        request_header = "{} {} {}\r\n".format(method, path, http_ver)
+        for key, value in request_headers.items():
+            tmp_str = "{}: {}\r\n".format(key, value)
+            request_header += tmp_str
+        request_header = (request_header + "\r\n").encode("utf-8")
+        print("request_header: ", request_header)
+        #print(request_header)
+        #print(type(request_header))
+        
+        s.send(request_header)
+        
+        #response = s.makefile("r", encoding="utf8", newline="\r\n")
+        response = s.makefile("rb", newline="\r\n")
+        print("response type: ", type(response))
 
-    # HTTP Version, Response Code(status), Response Description(ex OK)のような形で帰ってくる
-    statusline = response.readline()
-    print("statusline: ", type(statusline))
-    version, status, explanation = statusline.split(b" ", 2)
-    print("version: ",version, " status: ", status, " exp: ", explanation)
-    version = version.decode('utf-8')
-    status = status.decode('utf-8')
-    explanation = explanation.decode('utf-8')
-    print("version: ",version, " status: ", status, " exp: ", explanation)
-    assert status == "200", "{}: {}".format(status, explanation)
+        # HTTP Version, Response Code(status), Response Description(ex OK)のような形で帰ってくる
+        statusline = response.readline()
+        print("statusline: ", type(statusline))
+        print("statusline: ", statusline)
+        version, status, explanation = statusline.split(b" ", 2)
+        print("version: ",version, " status: ", status, " exp: ", explanation)
+        version = version.decode('utf-8')
+        status = status.decode('utf-8')
+        explanation = explanation.decode('utf-8')
+        print("version: ",version, " status: ", status, " exp: ", explanation)
+        assert status == "200" or status == "301", "{}: {}".format(status, explanation)
 
-    # ステータス行の次のヘッダーの処理
-    headers = {}
-    while True:
-        line = response.readline().decode('utf-8')
-        if line == "\r\n":
-            break
-        header, value = line.split(":", 1)
-        headers[header.lower()] = value.strip()
+        # ステータス行の次のヘッダーの処理
+        headers = {}
+        while True:
+            line = response.readline().decode('utf-8')
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            headers[header.lower()] = value.strip()
 
-    # アクセスしようとするデータが通常とは異なる方法で送信されていることを示すヘッダーがないかを確認
-    #assert "transfer-encoding" not in headers
-    #assert "content-encoding" not in headers
+        # アクセスしようとするデータが通常とは異なる方法で送信されていることを示すヘッダーがないかを確認
+        #assert "transfer-encoding" not in headers
+        #assert "content-encoding" not in headers
 
-    print(headers)
-    body = response.read()
-    #print(body)
-    if "content-encoding" in headers and headers["content-encoding"] == "gzip":
-        #body = gzip.decompress(body)
-        if "transfer-encoding" in headers:
-            body = chunked_text(body)
-        body = gzip.decompress(body)
-    body = body.decode("utf-8")
-    #print(body)
-    if is_view_source:
-        body = transform(body)
+        print(headers)
+        if status == "301":
+            url = headers["location"]
+            print("======")
+            print("url: ", url)
+            #scheme, url, host, path = urlparse(url)
+            #path = "/index.html"
+            #print("path: ", path)
+            #print("======")
+            continue
+        body = response.read()
+        #print(body)
+        if "content-encoding" in headers and headers["content-encoding"] == "gzip":
+            #body = gzip.decompress(body)
+            if "transfer-encoding" in headers:
+                body = chunked_text(body)
+            body = gzip.decompress(body)
+        body = body.decode("utf-8")
+        #print(body)
+        if is_view_source:
+            body = transform(body)
+        
+        break
+
     s.close()
 
     return headers, body
